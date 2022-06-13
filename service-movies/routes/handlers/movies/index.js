854e7f12d63486cd30b639c543c43e439f259901
Ivan Adrianto <ivanadrianto64@gmail.com>
2022-06-13 16:59:44 +0700

    remove routing get movies by title

--- service-movies/routes/handlers/movies/index.js
+++ service-movies/routes/handlers/movies/index.js
@@ -1,11 +1,9 @@
 const create = require("./create");
 const getAll = require("./getAll");
-const getByTitle = require("./getByTitle");
 const get = require("./get");
 
 module.exports = {
   create,
   getAll,
   get,
-  getByTitle,
 };
--- service-movies/routes/movies.js
+++ service-movies/routes/movies.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const moviesHandler = require("./handlers/movies");
 
 router.get("/", moviesHandler.getAll);
-router.get("/search", moviesHandler.getByTitle);
 router.get("/:id", moviesHandler.get);
 router.post("/", moviesHandler.create);
 
